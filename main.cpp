ad4d2e1bf5aac0eea44f1bab758d658da4048a9f
Trent_T <trenttuedon2000@gmail.com>
2026-06-02 12:48:43 -0400

    add font

--- main.cpp
+++ main.cpp
@@ -1,24 +1,52 @@
 #include "iostream"
 #include <cstdint>
 #include <fstream>
-#include <iomanip>
 
 unsigned char memory[4096]{};
+unsigned int startingAddress = 0x200;
 unsigned char* pc = memory;
 unsigned char display[64 * 32]{};
+
+void loadFont()
+{
+	unsigned int fontAddress = 0x050; 
+	unsigned char font_data[80] {
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+	for (int i = 0; i < 80; i++) 
+	{
+		memory[fontAddress + i] = font_data[i];
+		std::cout << std::hex << (int)memory[fontAddress + i] << '\n'; 
+	}
+}
 
 void loadROM(std::string_view filePath) 
 {
 	std::ifstream file{filePath.data(), std::ios::binary};
-	if (file.is_open()) 
+	int counter = 0; 
+	while(!file.eof())
 	{
-		file.read(reinterpret_cast<char*>(memory), 4096); 
-		if(file.gcount() > 0)
-		{
-			std::cout << "File Read Succesfully" << '\n';
-		}
+		unsigned char c = file.get();
+		memory[startingAddress + counter] = c;
+		counter++; 
 	}
-
+	pc += startingAddress;
+	loadFont();
 }
 
 uint16_t fetchInstructions() 
@@ -45,10 +73,34 @@ void clearScreen()
 
 void decode(uint16_t instruction)
 {
-	switch (instruction) 
+	uint16_t opcode = (instruction & 0xF000) >> 12;
+	switch (opcode) 
 	{
-		case 0x00E0: clearScreen();
-			
+		case 0x0:
+			std::cout << "Hello"; 
+		break;
+
+		case 0x1:
+			std::cout << "Hello"; 
+		break;
+
+		case 0x6:
+			std::cout << "Hello"; 
+		break;
+
+		case 0x7:
+			std::cout << "Hello"; 
+		break;
+
+		case 0xA:
+			std::cout << "Hello"; 
+		break;
+
+		case 0xD:
+			std::cout << "Hello"; 
+		break;
+
+
 	}
 }
 
@@ -57,12 +109,12 @@ int main()
 {
 	loadROM("./logo.ch8"); 
 	int counter = 0; 
-	while (counter != 10) 
-	{
-		uint16_t instruction = fetchInstructions(); 
-		std::cout << std::setw(4) << std::setfill('0') <<  std::hex << static_cast<uint16_t>(instruction) << '\n';
-		// decode(); 
-		// execute();
-		counter += 1; 
-	}
+	// while (counter != 10) 
+	// {
+	// 	uint16_t instruction = fetchInstructions(); 
+	// 	std::cout << std::setw(4) << std::setfill('0') <<  std::hex << static_cast<uint16_t>(instruction) << '\n';
+	// //	decode(instruction); 
+	// 	// execute();
+	// 	counter += 1; 
+	// }
 }
