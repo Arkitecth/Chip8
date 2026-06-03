55322a10203dad560b662af3882b9c5b68bdb132
Trent_T <trenttuedon2000@gmail.com>
2026-06-03 13:15:51 -0400

    add op_6xnn

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "iostream"
+#include <cstddef>
 #include <cstdint>
 #include <fstream>
 
 unsigned char memory[4096]{};
-unsigned int startingAddress = 0x200;
-unsigned char* pc = memory;
+uint16_t startingAddress = 0x200;
+uint16_t pc = startingAddress;
 unsigned char display[64 * 32]{};
+uint8_t vx[16]{};
 
 void loadFont()
 {
@@ -28,10 +30,10 @@ void loadFont()
 		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 	};
+
 	for (int i = 0; i < 80; i++) 
 	{
 		memory[fontAddress + i] = font_data[i];
-		std::cout << std::hex << (int)memory[fontAddress + i] << '\n'; 
 	}
 }
 
@@ -45,76 +47,84 @@ void loadROM(std::string_view filePath)
 		memory[startingAddress + counter] = c;
 		counter++; 
 	}
-	pc += startingAddress;
 	loadFont();
 }
 
 uint16_t fetchInstructions() 
 {
-	unsigned char byte = *pc; 
-
-	unsigned char byte_2= *(pc + 1); 
+	uint16_t instruction = (memory[pc] << 8) | memory[pc + 1];
 
 	pc += 2;
 
-	uint16_t result = (static_cast<uint16_t>(byte) << 8) | byte_2;
-
-	return result;
+	return instruction;
 }
 
-void clearScreen()
+void OP_00E0()
 {
 	for(int i = 0; i < 64 * 32; i++)
 	{
 		display[i] = 0; 
 	}
 }
+void OP_1NNN(uint16_t params) 
+{
+	pc = params;
+}
+
+void OP_6XNN(uint16_t params)
+{
+	std::cout << "First time entering this function" << '\n';
+	uint16_t regName = (params & 0x0F00) >> 8; 
+	uint16_t value = (params & 0x00FF); 
+	vx[regName] = value;
+	std::cout << static_cast<int>(regName) << '\n';
+}
 
 
 void decode(uint16_t instruction)
 {
 	uint16_t opcode = (instruction & 0xF000) >> 12;
+	uint16_t params = (instruction & 0x0FFF);
+
 	switch (opcode) 
 	{
 		case 0x0:
-			std::cout << "Hello"; 
+			OP_00E0(); 
 		break;
 
 		case 0x1:
-			std::cout << "Hello"; 
+			OP_1NNN(params);
 		break;
 
 		case 0x6:
-			std::cout << "Hello"; 
+			OP_6XNN(params); 
 		break;
 
-		case 0x7:
-			std::cout << "Hello"; 
-		break;
-
-		case 0xA:
-			std::cout << "Hello"; 
-		break;
-
-		case 0xD:
-			std::cout << "Hello"; 
-		break;
-
-
+		// case 0x7:
+		// 	std::cout << "Hello"; 
+		// break;
+		//
+		// case 0xA:
+		// 	std::cout << "Hello"; 
+		// break;
+		//
+		// case 0xD:
+		// 	std::cout << "Hello"; 
+		// break;
 	}
 }
+
 
 
 int main() 
 {
 	loadROM("./logo.ch8"); 
 	int counter = 0; 
-	// while (counter != 10) 
-	// {
-	// 	uint16_t instruction = fetchInstructions(); 
-	// 	std::cout << std::setw(4) << std::setfill('0') <<  std::hex << static_cast<uint16_t>(instruction) << '\n';
-	// //	decode(instruction); 
-	// 	// execute();
-	// 	counter += 1; 
-	// }
+	while (counter != 10) 
+	{
+		uint16_t instruction = fetchInstructions(); 
+		std::cout << std::hex << instruction << '\n';
+		decode(instruction); 
+		counter += 1; 
+	}
 }
